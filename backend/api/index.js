f2f43af04e04b21925af780df67a25196ec58f0f
StevenDelgadillo <sdelgadillov@ucenfotec.ac.cr>
2025-08-06 18:44:28 -0600

    Conflicto resuelto correctamente en index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,4 @@
-
-// Codigo de iniciar express de https://expressjs.com/
+// Código de iniciar express de https://expressjs.com/
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -11,15 +10,11 @@ require('dotenv').config();
 const routesUsuarios = require('./Routes/routesUsuarios.js');
 const routesEventos = require('./Routes/eventos');
 const rutaAnuncios = require('./Routes/routesAnuncios');
-<<<<<<< HEAD
 const rutasZonas = require('./Routes/comunidadRuta');
 const rutasPublicaciones = require('./Routes/publicaciones.js');
-=======
 const eventosRouter = require('./Routes/eventosLeo');
-// Conectarse a mongoose 
->>>>>>> a7c3fd75bca5c90451622cfee066999cbf1dfaac
 
-// Conexión a MongoDB
+// Conectarse a mongoose 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -39,8 +34,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routesUsuarios);
 app.use('/api/eventos', routesEventos);
 app.use('/api', rutaAnuncios);
+app.use('/api/zonas', rutasZonas);
+app.use('/api/publicaciones', rutasPublicaciones);
 app.use('/api/eventosLeo', eventosRouter);
 
+// Iniciar el servidor
 app.listen(port, () => {
-  console.log(`La aplicación esta corriendo en el puerto ${port}`);
+  console.log(`La aplicación está corriendo en el puerto ${port}`);
 });
