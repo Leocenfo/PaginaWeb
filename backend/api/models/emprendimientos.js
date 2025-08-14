540fe4a5b920e0ae874dbbd09a2546479936a9cf
Marco0924 <mgonzalezpe@ucenfotec.ac.cr>
2025-08-14 00:38:23 -0600

    Model empredimientos

---
+++ backend/api/models/emprendimientos.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+
+const emprendimientoSchema = new mongoose.Schema({
+    nombreCompleto: {type:String, required:true},
+    correo:{type:String, required:true, unique:true},
+    numTel:{type:String, required:true, unique:true},
+    tipoEmprendimiento:{type:String, required:true},
+    nomEmprendimiento:{type:String, required:true},
+    descripEmprendimiento:{type:String, required:true},
+    linkImagen: { type: String, required: true },
+    redesSociales: { type: String },   
+    estado:{type:String, default:'inactivo', emun:['inactivo','activo'] }
+})
+
+module.exports = mongoose.model('Emprendimiento',emprendimientoSchema)
