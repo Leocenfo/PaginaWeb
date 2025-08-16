0b09975760c73f145e7eacfbe0f9c7a06dfa3223
vbarrantesv <vbarrantesv@ucenfotec.ac.cr>
2025-08-15 19:27:10 -0700

    Se agrega cerrar sesion
    Front end y backend, se editan todos los headers, se refiere a los scripts y se crea el script en el folder de JS

---
+++ trabajo_LEO/pagina_404NOTFOUND/js/cerrarSesion.js
@@ -0,0 +1,14 @@
+    document.addEventListener("DOMContentLoaded", function () {
+        const btnCerrar = document.getElementById("BtnCerrarSesion");
+                console.log("cerrarSesion.js cargado");
+        if (btnCerrar) {
+            btnCerrar.addEventListener("click", function () {
+                // Eliminar la información del usuario del localStorage
+                localStorage.removeItem("usuarioLogueado");
+                // Devolver al usuario a la página de inicio
+                window.location.href = "../login_vale/home_Vale.HTML";
+            });
+        }
+    });
+
+    
